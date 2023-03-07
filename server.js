e941e8e67b3d61999fbd1cfabe359fbd9ae35f96
AndurGaitan <andresdavidagaitan@hotmail.com>
2023-03-06 23:25:29 -0300

    agregando tercera entrega

---
+++ server.js
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+import { connectDB } from './src/DAOs/createMongoConnection.js.js';
+import { startServer } from './app.js';
+import { executeServerCluster } from './src/utils/excetuteClusterMode.js';
+import { logger } from './src/utils/logger.js';
+import { config } from './src/constants/config.js';
+
+dotenv.config();
+
+const port  = process.env.PORT || 8080;
+
+connectDB();
+
+switch (config.executionMode) {
+    case "cluster":
+        logger.info("Executing app in cluster mode");
+        executeServerCluster(port)
+        break;
+
+    default:
+        logger.info("Executing app in fork mode");
+        startServer(port);
+        break;
+}
